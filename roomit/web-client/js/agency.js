b63f13837f6f6e881f90e687381d146395c73f9f
kkhrystosenko <kkhrystosenko@malkosua.com>
2015-04-29 23:33:53 +0300

    fix contact form

--- roomit/web-client/js/agency.js
+++ roomit/web-client/js/agency.js
@@ -29,14 +29,86 @@ function onload() {
         $('.navbar-toggle:visible').click();
     });
 
-    if(lang.lang == 'en'){
+    if (lang.lang == 'en') {
         $('#lang_en').hide();
         $('#lang_ru').show();
-    }
-    else {
+    } else {
         $('#lang_ru').hide();
         $('#lang_en').show();
     }
+
+    $(function() {
+
+        $("input,textarea").jqBootstrapValidation({
+            preventSubmit: true,
+            submitError: function($form, event, errors) {
+                console.log(errors);
+                // additional error messages or events
+            },
+            submitSuccess: function($form, event) {
+                event.preventDefault(); // prevent default submit behaviour
+                // get values from FORM
+                var name = $("input#name").val();
+                var email = $("input#email").val();
+                var phone = $("input#phone").val();
+                var message = $("textarea#message").val();
+                var firstName = name; // For Success/Failure Message
+                // Check for white space in name for Success/Fail message
+                if (firstName.indexOf(' ') >= 0) {
+                    firstName = name.split(' ').slice(0, -1).join(' ');
+                }
+                $.ajax({
+                    url: "/mail/contact_me.php",
+                    type: "POST",
+                    data: {
+                        name: name,
+                        phone: phone,
+                        email: email,
+                        message: message
+                    },
+                    cache: false,
+                    success: function() {
+                        // Success message
+                        $('#success').html("<div class='alert alert-success'>");
+                        $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                            .append("</button>");
+                        $('#success > .alert-success')
+                            .append("<strong>Your message has been sent. </strong>");
+                        $('#success > .alert-success')
+                            .append('</div>');
+
+                        //clear all fields
+                        $('#contactForm').trigger("reset");
+                    },
+                    error: function() {
+                        // Fail message
+                        $('#success').html("<div class='alert alert-danger'>");
+                        $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                            .append("</button>");
+                        $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                        $('#success > .alert-danger').append('</div>');
+                        //clear all fields
+                        $('#contactForm').trigger("reset");
+                    },
+                })
+            },
+            filter: function() {
+                return $(this).is(":visible");
+            },
+        });
+
+        $("a[data-toggle=\"tab\"]").click(function(e) {
+            e.preventDefault();
+            $(this).tab("show");
+        });
+    });
+
+
+    /*When clicking on Full hide fail/success boxes */
+    $('#name').focus(function() {
+        $('#success').html('');
+    });
+
 }
 
 var loadTemplate = function(templatePath) {
@@ -94,19 +166,22 @@ var getMs = function() {
  * $.parseParams - parse query string paramaters into an object.
  */
 (function(cash) {
-var re = /([^&=]+)=?([^&]*)/g;
-var decodeRE = /\+/g;  // Regex for replacing addition symbol with a space
-var decode = function (str) {return decodeURIComponent( str.replace(decodeRE, " ") );};
-$.parseParams = function(query) {
-    var params = {}, e;
-    while ( e = re.exec(query) ) { 
-        var k = decode( e[1] ), v = decode( e[2] );
-        if (k.substring(k.length - 2) === '[]') {
-            k = k.substring(0, k.length - 2);
-            (params[k] || (params[k] = [])).push(envy);
+    var re = /([^&=]+)=?([^&]*)/g;
+    var decodeRE = /\+/g; // Regex for replacing addition symbol with a space
+    var decode = function(str) {
+        return decodeURIComponent(str.replace(decodeRE, " "));
+    };
+    $.parseParams = function(query) {
+        var params = {},
+            e;
+        while (e = re.exec(query)) {
+            var k = decode(e[1]),
+                v = decode(e[2]);
+            if (k.substring(k.length - 2) === '[]') {
+                k = k.substring(0, k.length - 2);
+                (params[k] || (params[k] = [])).push(envy);
+            } else params[k] = v;
         }
-        else params[k] = v;
-    }
-    return params;
-};
+        return params;
+    };
 })(jQuery);
