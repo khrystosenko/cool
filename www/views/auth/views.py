ea552bd2a7a79625c8b58b61b67336a7cee77f05
Volodymyr <vovaminiof@gmail.com>
2015-05-06 22:28:29 +0200

    updated view

--- www/views/auth/views.py
+++ www/views/auth/views.py
@@ -1,17 +1,25 @@
 from django.shortcuts import redirect
+from django.template.response import TemplateResponse
 
 from ..utils import auth, request
 from ..utils.views import JSONResponse
 
+
 def signup(req):
-    data = auth.signup(request.get_params(req))
-    return JSONResponse(data, status=data.get('error_code', 200))
+    if req.method == 'POST':
+        data = auth.signup(request.get_params(req))
+        return JSONResponse(data, status=data.get('error_code', 200))
+
+    return TemplateResponse(request, 'signup.html')
 
 def login(req):
-    if auth.is_logged(req):
-        return redirect('home')
+    # if auth.is_logged(req):
+    #     return redirect('home')
         
-    return JSONResponse({'status': 'ok'})
+    return TemplateResponse(request, 'login.html')
 
-def logout(req):
-    return JSONResponse({'status': 'ok'})
+def logout(request):
+    return TemplateResponse(request, 'index.html')
+
+def profile(request):
+    return TemplateResponse(request, 'profile.html')
