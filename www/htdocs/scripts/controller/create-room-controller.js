1434e4d04943f2dd22fc125845687a92a9a8a7ff
kkhrystosenko <kkhrystosenko@malkosua.com>
2015-05-15 17:16:32 +0300

    when chat there is chat id -> call nodejs

--- www/htdocs/scripts/controller/create-room-controller.js
+++ www/htdocs/scripts/controller/create-room-controller.js
@@ -1,26 +1,47 @@
-$(function() {
+(function() {
 
     var API = APP.API;
 
     var CreateRoom = APP.CreateRoom;
+
+
+    var startChat = function() {
+        $.ajax({
+            type: "POST",
+            url: 'http://localhost:9999/chat',
+            data: JSON.stringify({}),
+            cache: false,
+            success: function() {
+
+            },
+            error: function() {
+
+            },
+        });
+    };
+
+    $(document).on('appRouteChange', function(){
+        // console.log(Nav.getParam());
+        startChat();
+    })
 
     CreateRoom.createRoom = function(e) {
 
         e.preventDefault();
 
         var data = {
-                username: $('#sender-name').val(),
-                username: $('#sender-link').val()
-                username: $('#sender-game').val()
-                username: $('#sender-language').val()
+            username: $('#sender-name').val(),
+            username: $('#sender-link').val(),
+            username: $('#sender-game').val(),
+            username: $('#sender-language').val()
         };
 
         API.CreateRoom(data).done(function(success) {
-        
+
             console.log("success");
 
         }).fail(function(errors) {
-            
+
             console.log(errors);
 
         });
---
+++ www/node/server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+
+var nodemailer = require("nodemailer");
+var smtpTransport = require('nodemailer-smtp-transport');
+
+var app = express(),
+    server = require('http').createServer(app);
+
+app.use(bodyParser.json({
+    limit: '50mb'
+}));
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.post('/chat', function (req, res) {
+    console.log('chat');
+});
+
+var port = 9999,  
+    ip = "127.0.0.1";
+
+server.listen(port, ip, function() {console.log('Server is running.')});
