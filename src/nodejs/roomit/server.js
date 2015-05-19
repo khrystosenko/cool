634d49117c8f6831fac1b80c9d43fc219961159d
kkhrystosenko <kkhrystosenko@malkosua.com>
2015-05-19 10:46:15 +0300

    sockets

--- src/nodejs/roomit/server.js
+++ src/nodejs/roomit/server.js
@@ -1,5 +1,16 @@
-var io = require('socket.io').listen(7000); 
+var io = require('socket.io').listen(8888);
 
-io.sockets.on('connection', function (socket) {
-	console.log('yeah');
+
+  console.log('a user connected');
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    socket.broadcast.emit(msg);
+    io.emit('chat message', msg);
+  });
+
 });
