69125f90a7c62948f76dcfc6d7139c8fe445687e
kkhrystosenko <kkhrystosenko@malkosua.com>
2015-05-19 13:59:41 +0300

    socket.io chat example

--- src/nodejs/roomit/server.js
+++ src/nodejs/roomit/server.js
@@ -1,17 +1,84 @@
 var io = require('socket.io').listen(8888);
 
-io.sockets.on('connection', function(socket){
+// io.sockets.on('connection', function(socket){
 
-  console.log('a user connected');
+//   console.log('a user connected');
 
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+//   socket.on('disconnect', function(){
+//     console.log('user disconnected');
+//   });
+
+//   socket.on('chat message', function(msg){
+//     console.log('message: ' + msg);
+//     socket.broadcast.emit(msg);
+//     io.emit('chat message', msg);
+//   });
+
+// });
+// Chatroom
+
+// usernames which are currently connected to the chat
+var usernames = {};
+var numUsers = 0;
+
+io.sockets.on('connection', function (socket) {
+  var addedUser = false;
+
+  // when the client emits 'new message', this listens and executes
+  socket.on('new message', function (data) {
+    // we tell the client to execute 'new message'
+    console.log(socket.username, data)
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    });
   });
 
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-    socket.broadcast.emit(msg);
-    io.emit('chat message', msg);
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', function (username) {
+  	console.log(username);
+    // we store the username in the socket session for this client
+    socket.username = username;
+    // add the client's username to the global list
+    usernames[username] = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
   });
 
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('typing', function () {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
+  });
+
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', function () {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', function () {
+    // remove the username from global usernames list
+    if (addedUser) {
+      delete usernames[socket.username];
+      --numUsers;
+
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    }
+  });
 });
