dfd4464aa361f148cda7f2896a0f67206f949777
Vova Minof <vovaminiof@gmail.com>
2015-06-09 14:50:55 +0200

    Output mic level to the console.

--- www/htdocs/scripts/controller/network.js
+++ www/htdocs/scripts/controller/network.js
@@ -60,6 +60,7 @@
     } else {
       console.log("Browser does not appear to be WebRTC-capable");
     }
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
 
     // Initialize varibles
@@ -294,7 +295,7 @@
 
     }
 
-    function setup_local_media(callback, errorback) {
+    function setup_local_media(callback, audio_level_callback) {
         if (localMediaStream != null) {  /* ie, if we've already been initialized */
             if (callback) callback();
             return;
@@ -308,6 +309,34 @@
                 local_media.attr('muted', true); /* always mute ourselves by default */
                 $('#' + audioChatID).append(local_media);
                 navigator.attachMediaStream(local_media[0], stream);
+
+                var audioContext = new webkitAudioContext(),
+                    analyser = audioContext.createAnalyser(),
+                    microphone = audioContext.createMediaStreamSource(stream),
+                    javascriptNode = audioContext.createScriptProcessor(2048, 1, 1);
+
+                analyser.smoothingTimeConstant = 0.3;
+                analyser.fftSize = 1024;
+
+                microphone.connect(analyser);
+                analyser.connect(javascriptNode);
+                javascriptNode.connect(audioContext.destination);
+
+                javascriptNode.onaudioprocess = function() {
+                    var array =  new Uint8Array(analyser.frequencyBinCount);
+                    analyser.getByteFrequencyData(array);
+                    var values = 0;
+
+                    var length = array.length;
+                    for (var i = 0; i < length; i++) {
+                        values += array[i];
+                    }
+
+                    var average = values / length;
+                    console.log(average);
+                }
+
+
                 if (callback) callback(true);
             },
             function() { /* user denied access to a/v */
