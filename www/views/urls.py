0cd7ceed51c00c14fcce59ed0d2f17dbe11fa9c1
Vova Minof <vova.minof@investio.pl>
2015-08-11 11:31:23 +0200

    Added robots.txt and sitemap.xml

--- www/views/urls.py
+++ www/views/urls.py
@@ -1,12 +1,16 @@
+from django.views.generic import TemplateView
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.conf.urls.static import static
 
 basepatterns = patterns('views.views',
-	url(r'^$', 'index', name='index'),
+    url(r'^$', 'index', name='index'),
 )
 
 urlpatterns = basepatterns + patterns('',
+    url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
+    url(r'^sitemap\.xml$', TemplateView.as_view(template_name='sitemap.xml', content_type='text/xml')),
+    
     url(r'^auth/', include('views.auth.urls', 'auth')),
     url(r'^room/', include('views.room.urls', 'room')),
     url(r'^feedback/', include('views.feedback.urls', 'feedback')),
