7185fc250619bc43668604d3bf7ed2df36032567
Vova Minof <vova.minof@investio.pl>
2015-08-12 10:28:42 +0200

    Added .sh script

--- src/python/roomit/bin/twitch_getter.py
+++ src/python/roomit/bin/twitch_getter.py
@@ -3,10 +3,13 @@ import time
 import requests
 
 # Uncomment to run locally
-import sys, os
-sys.path.insert(1, os.getcwd() + '\\..\\..')
-sys.path.insert(1, os.getcwd() + '\\..\\..\\..\\..\\www')
-os.environ["DJANGO_SETTINGS_MODULE"] = "settings"
+# import sys, os
+# sys.path.insert(1, os.getcwd() + '\\..\\..')
+# sys.path.insert(1, os.getcwd() + '\\..\\..\\..\\..\\www')
+# or
+# sys.path.insert(1, os.getcwd() + '/../..')
+# sys.path.insert(1, os.getcwd() + '/../../../../www')
+# os.environ["DJANGO_SETTINGS_MODULE"] = "settings"
 
 from django.conf import settings
 
