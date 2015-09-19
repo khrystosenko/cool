5410f83f225965fd0d4f5a6d4098c918be208470
Volodymyr <vovaminiof@gmail.com>
2015-09-19 20:30:28 +0200

    Fixing CREATE_DB.sq;

--- db/roomit/CREATE_DB.sql
+++ db/roomit/CREATE_DB.sql
@@ -2,10 +2,7 @@ use roomit;
 
 source schema/rooms.sql;
 source schema/feedbacks.sql;
-<<<<<<< Updated upstream
 source schema/rooms_history.sql;
-=======
 source schema/services.sql;
 source schema/games.sql;
 source schema/streams.sql;
->>>>>>> Stashed changes
