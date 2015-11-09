50749f45b8429dfd325b3c162d5f1d3f163d2386
kkhrystosenko <kkhrystosenko@malkosua.com>
2015-11-09 14:14:52 +0200

    Prerelease template adding

--- www/views/views.py
+++ www/views/views.py
@@ -2,6 +2,9 @@ from django.template.response import TemplateResponse
 
 from utils import search
 
-def index(req):
+# def index(req):
+#     top_streams = search.filter_by_params({'limit': '6'})
+#     return TemplateResponse(req, 'index.html', {'streams': top_streams['data']})
+def prerelease(req):
     top_streams = search.filter_by_params({'limit': '6'})
-    return TemplateResponse(req, 'index.html', {'streams': top_streams['data']})
+    return TemplateResponse(req, 'prerelease.html', {'streams': top_streams['data']})
