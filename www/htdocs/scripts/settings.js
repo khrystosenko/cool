6b381a6018f87e3cad7105ebaaa4dc5c2d6b87b2
Volodymyr <vovaminiof@gmail.com>
2015-11-13 01:58:22 +0100

    Fix

--- www/htdocs/scripts/settings.js
+++ www/htdocs/scripts/settings.js
@@ -1,6 +1,6 @@
 var HOST = window.location.origin;
 
 var SIGNALING_SERVER = {
-        HOST: HOST.split(':')[0] + ':' + HOST.split(':')[1];,
+        HOST: HOST.split(':')[0] + ':' + HOST.split(':')[1],
         PORT: 8888
     }
